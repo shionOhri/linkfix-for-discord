63fa1baf94aa29e2441fe2ae8ecd42711fe2b164
shionOhri <87513836+shionOhri@users.noreply.github.com>
2023-11-27 13:11:40 -0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ client.on(Events.MessageCreate, (message) => {
     if (message.content.includes(identifier)) {
       const result = replacer(message.content);
       if (result) {
-        reply += "\|\|" + result + "\|\|\n";
+        reply += result + "\|\|" + "\n";
       }
     }
   }
