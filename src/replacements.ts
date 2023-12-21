c610f5484f33fb17bba9c25d5e5628b004227763
shionOhri <87513836+shionOhri@users.noreply.github.com>
2023-12-21 15:25:26 -0800

    Fix vm.tiktok

--- src/replacements.ts
+++ src/replacements.ts
@@ -1,6 +1,5 @@
 // import dotenv from "dotenv";
 // dotenv.config();
-import re;
 import requests;
 
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'};
