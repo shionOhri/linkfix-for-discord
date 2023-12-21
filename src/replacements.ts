dbb87a7247b8acc0a39603aa3ff14d856d0008de
shionOhri <87513836+shionOhri@users.noreply.github.com>
2023-12-21 14:51:33 -0800

    Fix for vm.tiktok links

--- src/replacements.ts
+++ src/replacements.ts
@@ -26,7 +26,7 @@ function fixInstagramURL(content: string): string {
 }
 
 function fixTikTokURL(content: string): string {
-  let c = content.replace(/(www\.)?(vm\.)?(tiktok.com\/)/, "vxtiktok.com/");
+  let c = content.replace(/(www\.)?(tiktok.com\/)/, "vxtiktok.com/");
   c = c.replace(/\?.*/, "");
 
   return c;
@@ -72,6 +72,14 @@ export const replacements: {
     }
   },
   "//www.tiktok.com/": (content) => {
+    const urls = getUrls(content, /https?:\/\/(www\.)?tiktok\.com\/[^\s]+/g);
+    if (urls.length > 0) {
+      return urls.map((url) => fixTikTokURL(url)).join("\n");
+    } else {
+      return null;
+    }
+  },
+  "//vm.tiktok.com/": (content) => {
     const urls = getUrls(content, /https?:\/\/(www\.)?tiktok\.com\/[^\s]+/g);
     if (urls.length > 0) {
       return urls.map((url) => fixTikTokURL(url)).join("\n");
