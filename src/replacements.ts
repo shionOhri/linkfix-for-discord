ec4e72cb0d039a54b3d50602ef7496756ccd4af7
shionOhri <87513836+shionOhri@users.noreply.github.com>
2023-12-21 23:44:23 -0800

    Update replacements.ts

--- src/replacements.ts
+++ src/replacements.ts
@@ -1,7 +1,3 @@
-const axios = require("axios");
-// import dotenv from "dotenv";
-// dotenv.config();
-// NOTE: regexp must have the 'g' flag or else `matchAll` will throw
 function getUrls(content: string, regexp: RegExp): Array<string> {
   const urls: Array<string> = [];
   for (const matches of content.matchAll(regexp)) {
@@ -11,50 +7,58 @@ function getUrls(content: string, regexp: RegExp): Array<string> {
   return urls;
 }
 
+/*
+ * Link fixing functions
+ */
+
+function fixTwitterURL(content: string): string {
+  let c = content.replace(
+    /\/\/(x|twitter)\.com\//,
+    `//${process.env.TWITTER_FIX_URL}/`,
+  );
+  c = c.replace(/\?.*/, "");
+
+  return c;
+}
+
 function fixYTShortsURL(content: string): string {
-  let c = content.replace(/(www\.)?(youtube.com\/shorts\/)/, "youtu.be/");
+  let c = content.replace(
+    /(www\.)?(youtube\.com\/shorts\/)/,
+    `${process.env.YOUTUBE_FIX_URL}/`,
+  );
   c = c.replace(/\?.*/, "");
 
   return c;
 }
 
 function fixInstagramURL(content: string): string {
-  let c = content.replace(/(www\.)?(instagram.com\/)/, "ddinstagram.com/");
-  c = c.replace(/\?.*/, "");
+  let c = content.replace(
+    /(www\.)?(instagram\.com\/)/,
+    `${process.env.INSTAGRAM_FIX_URL}/`,
+  );
 
   return c;
 }
 
 function fixTikTokURL(content: string): string {
-  let c = content.replace(/(www\.)?(tiktok.com\/)/, "vxtiktok.com/");
-  c = c.replace(/\?.*/, "");
+  let c = content.replace(/(tiktok\.com\/)/, `${process.env.TIKTOK_FIX_URL}/`);
 
   return c;
 }
 
-function fixVMTikTokURL(content: string): string {
-  let c = "";
-  axios
-    .get(content)
-    .then(function (response) {
-      c = response.request.res.responseURL;
-    })
-    .catch(function (no200) {
-      console.error("400, 404, and other events");
-    });
-  c = content.replace(/(www\.)?(tiktok.com\/)/, "vxtiktok.com/");
-  c = c.replace(/\?.*/, "");
-  return c;
-}
+function fixRedditURL(content: string): string {
+  let c: string;
 
-function fixTwitterURL(content: string): string {
-  let c = content.replace(
-    /\/\/(x|twitter).com\//,
-    `//${process.env.FXTWITTER_URL}/`,
-  );
-  // TODO: Configure a more robust way of stripping all the tracking crap off
-  //       of YouTube URLS (currently `(?|&)si=<trackingid>`)
-  c = c.replace(/\?.*/, "");
+  if (content.includes("reddit.com")) {
+    c = content.replace(
+      /((www|old|new|np)\.)?reddit\.com\//,
+      `${process.env.REDDIT_FIX_URL}/`,
+    );
+  } else if (content.includes("redd.it")) {
+    c = content.replace(/redd\.it\//, `${process.env.REDDIT_FIX_URL}/`);
+  } else {
+    throw new Error(`Could not determine a replacement for ${content}`);
+  }
 
   return c;
 }
@@ -62,50 +66,76 @@ function fixTwitterURL(content: string): string {
 export const replacements: {
   [identifier: string]: (content: string) => string | null;
 } = {
-  "//x.com/": (content) => {
-    const urls = getUrls(content, /https?:\/\/x\.com\/[^\s]+/g);
-    if (urls.length > 0) {
+  "x.com/": (content) => {
+    const urls = getUrls(
+      content,
+      /https?:\/\/x\.com\/(\w){1,15}\/status\/[^\s]+/g,
+    );
+    if (process.env.TWITTER_FIX_URL && urls.length > 0) {
       return urls.map((url) => fixTwitterURL(url)).join("\n");
     } else {
       return null;
     }
   },
-  "//twitter.com/": (content) => {
-    const urls = getUrls(content, /https?:\/\/twitter\.com\/[^\s]+/g);
-    if (urls.length > 0) {
+  "twitter.com/": (content) => {
+    const urls = getUrls(
+      content,
+      /https?:\/\/twitter\.com\/(\w){1,15}\/status\/[^\s]+/g,
+    );
+    if (process.env.TWITTER_FIX_URL && urls.length > 0) {
       return urls.map((url) => fixTwitterURL(url)).join("\n");
-    } else {
-      return null;
-    }
-  },
-  "//www.instagram.com/": (content) => {
-    const urls = getUrls(content, /https?:\/\/(www\.)?instagram\.com\/[^\s]+/g);
-    if (urls.length > 0) {
-      return urls.map((url) => fixInstagramURL(url)).join("\n");
-    } else {
-      return null;
-    }
-  },
-  "//www.tiktok.com/": (content) => {
-    const urls = getUrls(content, /https?:\/\/(www\.)?tiktok\.com\/[^\s]+/g);
-    if (urls.length > 0) {
-      return urls.map((url) => fixTikTokURL(url)).join("\n");
-    } else {
-      return null;
-    }
-  },
-  "//vm.tiktok.com/": (content) => {
-    const urls = getUrls(content, /https?:\/\/(vm\.)?tiktok\.com\/[^\s]+/g);
-    if (urls.length > 0) {
-      return urls.map((url) => fixVMTikTokURL(url)).join("\n");
     } else {
       return null;
     }
   },
   "youtube.com/shorts/": (content) => {
     const urls = getUrls(content, /https?:\/\/(www\.)?youtube\.com\/[^\s]+/g);
-    if (urls.length > 0) {
+    if (process.env.YOUTUBE_FIX_URL && urls.length > 0) {
       return urls.map((url) => fixYTShortsURL(url)).join("\n");
+    } else {
+      return null;
+    }
+  },
+  "instagram.com/": (content) => {
+    const urls = getUrls(
+      content,
+      /https?:\/\/(www\.)?instagram\.com\/[^\s]+/g,
+    );
+    if (process.env.INSTAGRAM_FIX_URL && urls.length > 0) {
+      return urls.map((url) => fixInstagramURL(url)).join("\n");
+    } else {
+      return null;
+    }
+  },
+  "tiktok.com/": (content) => {
+    const urls = getUrls(
+      content,
+      /https?:\/\/((www|vm)\.)?tiktok\.com\/[^\s]+/g,
+    );
+    if (process.env.TIKTOK_FIX_URL && urls.length > 0) {
+      return urls.map((url) => fixTikTokURL(url)).join("\n");
+    } else {
+      return null;
+    }
+  },
+  "reddit.com/": (content) => {
+    const urls = getUrls(
+      content,
+      /https?:\/\/((www|old|new|np)\.)?reddit\.com\/[^\s]+/g,
+    );
+    if (process.env.REDDIT_FIX_URL && urls.length > 0) {
+      return urls.map((url) => fixRedditURL(url)).join("\n");
+    } else {
+      return null;
+    }
+  },
+  "redd.it/": (content) => {
+    const urls = getUrls(
+      content,
+      /https?:\/\/redd\.it\/[^\s]+/g,
+    );
+    if (process.env.REDDIT_FIX_URL && urls.length > 0) {
+      return urls.map((url) => fixRedditURL(url)).join("\n");
     } else {
       return null;
     }
